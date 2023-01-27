31841dfc70808078af53ba542c069598c98c7a8d
leonardog9823 <guanuco43@gmail.com>
2023-01-26 21:22:22 -0300

    document.ts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Raleway:wght@400;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
